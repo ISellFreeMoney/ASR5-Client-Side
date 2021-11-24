7c6e2d50fe89395670231a0bcc46ded54e547a9b
Victor LAUMOND <mineagmerfr@gmail.com>
2021-11-24 11:57:53 +0100

    Fin de la configuration client

--- client.c
+++ client.c
@@ -3,7 +3,6 @@
 #include <strings.h>
 #include <stdlib.h>
 
-
 int main(int argc, char *argv[])
 {
     struct sockaddr_in sa;
@@ -11,12 +10,23 @@ int main(int argc, char *argv[])
     struct servent *sp; // a obtenir en lisant le fichier services
     int s;
     hp = gethostbyname(argv[1]);
-    printf("%d\n",hp->h_addr[0]);
+    printf("%d\n", hp->h_addr[0]);
     sp = getservbyname("msp", "tcp");
     printf("%d\n", sp->s_port);
 
     s = socket(hp->h_addrtype, SOCK_STREAM, 0);
     printf("%d\n", s);
+
+    sa.sin_family = hp->h_addrtype;
+    sa.sin_port = sp->s_port;
+    bcopy(hp->h_addr, (char *)&sa.sin_addr, hp->h_length);
+
+    int c = connect(s, (struct sockaddr *)&sa, sizeof(sa));
+
+    if (c < 0)
+    {
+        perror("connect");
+    }
 
     return 0;
 }
