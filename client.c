f92a9f2b9d5318855de684b37f8e2f952288f742
Victor LAUMOND <mineagmerfr@gmail.com>
2021-11-24 11:32:41 +0100

    Avancement

--- client.c
+++ client.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
-#include <netinet/in.h>
+#include <netdb.h>
+#include <strings.h>
+#include <stdlib.h>
 
-// On declare: 
-struct sockaddr_in sa;
-struct hostend *hp; // a obtenir en lisant le fichier host
-struct servent *sp; // a obtenir en lisant le fichier services
 
-// On sait que
-typedef struct sockaddr_in{
-    short sin_family; // famille d'adresse : AF_INET
-    unsigned short sin_port; // port dqns l'ordre d'octets reseau
-    struct in_addr sin_addr; // Adresse Internet
-    char sin_zero[8];
-};
+int main(int argc, char *argv[])
+{
+    struct sockaddr_in sa;
+    struct hostent *hp; // a obtenir en lisant le fichier host
+    struct servent *sp; // a obtenir en lisant le fichier services
+    int s;
+    hp = gethostbyname(argv[1]);
+    printf("%d\n",hp->h_addr[0]);
+    sp = getservbyname("msp", "tcp");
+    printf("%d\n", sp->s_port);
 
-struct in_addr{
-    uint32_t s_addr; // adresse dans l'ordre d'octets reseau
-};
+    s = socket(hp->h_addrtype, SOCK_STREAM, 0);
+    printf("%d\n", s);
+
+    return 0;
+}
